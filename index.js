cfa90ae32f35d69604968c8f4919b36c1cb1c1b0
Benjamin Kopp <kopp@mecodia.de>
2017-12-04 13:31:31 +0100

    add holder and logic

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+app.get("/chef", function(req, res) {
+  res.sendFile(__dirname + "/chef.html");
+});
+
+io.on("connection", function(socket) {
+  socket.on("card draw", function(msg) {
+    io.emit("card draw", msg);
+  });
+
+  socket.on("card reset", function(msg) {
+    io.emit("card reset");
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
