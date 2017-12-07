d93074b2362543869f45277ee8059fab5b2550b3
Benjamin Kopp <kopp@mecodia.de>
2017-12-07 15:58:51 +0100

    add fancy animations and stack cards

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3002;
 
 app.use(express.static("public"));
 
