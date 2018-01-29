2f5b418d707377a8575932fc7cc4ec9e48d8775c
benjy3gg <benjy3gg@googlemail.com>
2018-01-30 00:42:59 +0100

    new design, moved to vue for chef view

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ app.get("/chef", function(req, res) {
   res.sendFile(__dirname + "/chef.html");
 });
 
+app.get("/chef2", function(req, res) {
+  res.sendFile(__dirname + "/chef2.html");
+});
+
 io.on("connection", function(socket) {
-  socket.on("card draw", function(msg) {
+  
+  socket.on("user vote", function(msg) {
     var currentRoom = socket.rooms[Object.keys(socket.rooms)[0]];
-    console.log(currentRoom);
-    io.sockets.in(currentRoom).emit("card draw", msg);
+    console.log(msg);
+    io.sockets.in(currentRoom).emit("user voted", msg);
   });
 
   socket.on("card reset", function(msg) {
@@ -27,9 +32,15 @@ io.on("connection", function(socket) {
     io.sockets.in(currentRoom).emit("card reset");
   });
 
-  socket.on("room", function(room) {
-    socket.join(room);
-    io.sockets.in(room).emit("user connected");
+  socket.on("room", function(user) {
+    socket.join(user.roomCode);
+    console.log(user);
+    io.sockets.in(user.roomCode).emit("user connected", user);
+  });
+
+  socket.on("room chef", function(roomCode) {
+    console.log("chef enters room: " + roomCode);
+    socket.join(roomCode);
   });
 
   socket.on("chef connected", function(room) {
