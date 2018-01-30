b31df40cf61eb49b6a6b341f5f94d7dd87bf0290
Benjamin Kopp <benjamin.kopp@scandio.de>
2018-01-30 17:37:37 +0100

    fixes

--- index.js
+++ index.js
@@ -6,53 +6,53 @@ var port = process.env.PORT || 3002;
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/chef", function(req, res) {
+app.get("/chef", function (req, res) {
   res.sendFile(__dirname + "/chef.html");
 });
 
-app.get("/chef2", function(req, res) {
+app.get("/chef2", function (req, res) {
   res.sendFile(__dirname + "/chef2.html");
 });
 
-io.on("connection", function(socket) {
-  
-  socket.on("user vote", function(msg) {
+io.on("connection", function (socket) {
+
+  socket.on("user vote", function (msg) {
     var currentRoom = socket.rooms[Object.keys(socket.rooms)[0]];
     console.log(msg);
     io.sockets.in(currentRoom).emit("user voted", msg);
   });
 
-  socket.on("card reset", function(msg) {
+  socket.on("card reset", function (msg) {
     var currentRoom = socket.rooms[Object.keys(socket.rooms)[0]];
     console.log(currentRoom);
     io.sockets.in(currentRoom).emit("card reset");
   });
 
-  socket.on("room", function(user) {
+  socket.on("room", function (user) {
     socket.join(user.roomCode);
     console.log(user);
     io.sockets.in(user.roomCode).emit("user connected", user);
   });
 
-  socket.on("room chef", function(roomCode) {
+  socket.on("room chef", function (roomCode) {
     console.log("chef enters room: " + roomCode);
     socket.join(roomCode);
   });
 
-  socket.on("chef connected", function(room) {
+  socket.on("chef connected", function (room) {
     io.sockets.in(room).emit("chef connected");
   });
 
-  socket.on("set sizes", function(sizes) {
+  socket.on("set sizes", function (sizes) {
     var currentRoom = socket.rooms[Object.keys(socket.rooms)[0]];
     io.sockets.in(currentRoom).emit("set sizes", sizes);
   });
 });
 
-http.listen(port, function() {
+http.listen(port, '0.0.0.0', function () {
   console.log("listening on *:" + port);
 });
