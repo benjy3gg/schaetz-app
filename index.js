2dd58e0e5bd67c429d3bc2b0b547d4d464518287
Benjamin Kopp <benjamin.kopp@scandio.de>
2018-01-31 18:57:00 +0100

    - fix issues with roomCode and baseUrl - fix disconnect issues with wrong connected user count

--- index.js
+++ index.js
@@ -18,29 +18,54 @@ app.get("/chef2", function (req, res) {
   res.sendFile(__dirname + "/chef2.html");
 });
 
+var rooms = [];
+
 io.on("connection", function (socket) {
 
-  socket.on("user vote", function (msg) {
-    var currentRoom = socket.rooms[Object.keys(socket.rooms)[0]];
-    console.log(msg);
+  socket.on("user can vote", function (user) {
+    var currentRoom = socket.rooms[user.roomCode];
+    io.sockets.in(currentRoom).emit("user can vote", user);
+  });
+
+  socket.on("user send vote", function (msg) {
+    var currentRoom = socket.rooms[msg.user.roomCode];
     io.sockets.in(currentRoom).emit("user voted", msg);
   });
 
-  socket.on("card reset", function (msg) {
-    var currentRoom = socket.rooms[Object.keys(socket.rooms)[0]];
+  socket.on("user wait", function (user) {
+    var currentRoom = socket.rooms[user.roomCode];
+    io.sockets.in(currentRoom).emit("user wait", user);
+  });
+
+  socket.on("card reset", function (roomCode) {
+    var currentRoom = socket.rooms[roomCode];
     console.log(currentRoom);
     io.sockets.in(currentRoom).emit("card reset");
   });
 
   socket.on("room", function (user) {
     socket.join(user.roomCode);
-    console.log(user);
+    //rooms[user.roomCode].users[user.name] = { user, socket };
+    //rooms[user.roomCode].sockets[socket.id] = { user, socket };
+    console.log("user enters room: " + user.roomCode);
     io.sockets.in(user.roomCode).emit("user connected", user);
   });
 
   socket.on("room chef", function (roomCode) {
+    //rooms[roomCode] = { id: roomCode, users: {}, sockets: {} };
     console.log("chef enters room: " + roomCode);
+    rooms.push(roomCode);
     socket.join(roomCode);
+  });
+
+  socket.on("new ticket", function (msg) {
+    io.sockets.in(msg.roomCode).emit("set ticket", msg.ticket);
+  })
+
+  socket.on('disconnecting', (reason) => {
+    Object.entries(socket.rooms).forEach(([key, value]) => {
+      io.sockets.in(key).emit("user disconnected", key);
+    });
   });
 
   socket.on("chef connected", function (room) {
